dd711f74a435fdbdaa41f2a04f89bda2277d68b6
VitorLippi2 <vitor.lippi@viannasempre.com.br>
2026-04-09 19:37:37 -0300

    finance criado

---
+++ pandas/finance.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+
+tkr =  yf.Ticker('PETR4.SA')
+hist = tkr.history(period="5d")
+
+hist = hist.drop("Dividends", axis=1)
+hist = hist.drop("Stock Splits", axis=1)
+
+hist = hist.reset_index()
+print(hist)
