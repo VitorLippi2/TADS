6101b3b60450734228f8fc959c096e900ab18785
VitorLippi2 <vitor.lippi@viannasempre.com.br>
2026-04-16 20:15:00 -0300

    tads - pandas

---
+++ dataframes/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+nba = pd.read_csv('nba.csv', sep=',')
+
+
+nba.insert(3, "Sport", value="Basktball")
+# print(nba.columns)
+
+nba = nba.dropna()
+
+
+# média salarial por colegiado dos jogadores
+nba = nba[["College", "Salary"]]
+nba = nba.groupby(["College"]).mean("Salary")
+
+print(nba)
