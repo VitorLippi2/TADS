3a43b12770d9697c868f17110ff05ee970b545de
VitorLippi2 <vitor.lippi@viannasempre.com.br>
2026-04-30 20:08:28 -0300

    aula

---
+++ dataframes/aula30_04/employees.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv('../employees.csv')
+df["Start Date"] = pd.to_datetime(df["Start Date"])
+df["Senior Management"] = df["Senior Management"].astype("bool")
+
+# limpando linhas nulas
+df = df.dropna(how='all')
+df['Team'] = df['Team'].fillna('Nulo')
+
+# novo df -> salário superior a 100_000
+mask = df["Salary"] > 100_000
+employees_over_100_000 = df[mask]
+print(employees_over_100_000)
+
+df_salary = df.groupby("Team").mean("Salary")
+df_salary = df[["Salary"]]
+print(df_salary)
+print(df_salary.nlargest(n=1, columns="Salary"))
+
+# salário por equipe dividido por gênero
+df_generos = df.groupby(["Team", "Gender"])["First Name"].count()
+print(df_generos)
