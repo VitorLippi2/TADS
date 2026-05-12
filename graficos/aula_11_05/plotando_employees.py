2147e11f6b286d65706dbcfc853983a2443752f9
VitorLippi2 <vitor.lippi@viannasempre.com.br>
2026-05-11 21:58:13 -0300

    subplots

--- graficos/aula_11_05/plotando_employees.py
+++ graficos/aula_11_05/plotando_employees.py
@@ -8,7 +8,6 @@ df = pd.read_csv("../employees.csv")
 fig, (ax, ax1) = plt.subplots(1,2)
 fig.set_size_inches(5.6, 4.2)
 
-
 # pegando valores minimos e máximos
 print(df["Salary"].nlargest(1))
 print(df["Salary"].nsmallest(1))
@@ -19,8 +18,14 @@ ax.hist(
 
 formatter = ticker.FormatStrFormatter("$%1.0f")
 ax.xaxis.set_major_formatter(formatter)
-plt.title("Salários mensais no departamento de vendas. (employees.csv)")
+ax.set_title("Salários mensais no departamento de vendas. (employees.csv)")
 plt.xlabel("Salário (bin = 10_000)")
 plt.ylabel("Frequência")
+
+# segundo gráfico
+df["Gender"] = df["Gender"].fillna("NI")
+group_gender = df.groupby("Gender")["Salary"].mean()
+ax1.pie(group_gender, labels=group_gender.index, autopct='%1.1f%%')
+
 
 plt.show()
